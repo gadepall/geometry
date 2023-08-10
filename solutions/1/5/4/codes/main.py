17109d128989e43bc22d6421839c3953bbb6c2c1
Antalene_J <119392158+Antalene-J@users.noreply.github.com>
2023-08-11 02:58:07 +0530

    Add files via upload

--- solutions/1/5/4/codes/main.py
+++ solutions/1/5/4/codes/main.py
@@ -1,33 +1,11 @@
 import numpy as np
 
-A = np.array([1, -1])
-B = np.array([-4, 6])
-C = np.array([-3, -5])
-omat = np.array([[0, 1], [-1, 0]])
 
-def dir_vec(A, B):
-    return B - A
 
-def norm_vec(A, B):
-    return omat @ dir_vec(A, B)
+I=(1/(np.sqrt(37) + 4 + np.sqrt(61)))*np.array([[np.sqrt(61) - 16 - 3*np.sqrt(37)],[-np.sqrt(61) + 24 - 5*np.sqrt(37)]]) #incnetre
+n = np.array([[11], [1]]) #normal vector 
+nT = n.T #transpose of normal
+r = abs((nT @ I) - 50)/(np.linalg.norm(n)) #distance
 
-k1=1
-k2=1
+print("distance is",r)
 
-p = np.zeros(2)
-t = norm_vec(B, C)
-n1 = t / np.linalg.norm(t)
-t = norm_vec(C, A)
-n2 = t / np.linalg.norm(t)
-t = norm_vec(A, B)
-n3 = t / np.linalg.norm(t)
-
-p[0] = n1 @ B - k1 * n2 @ C
-p[1] = n2 @ C - k2 * n3 @ A
-
-N = np.block([[n1 - k1 * n2],[ n2 - k2 * n3]])
-I = np.linalg.inv(N)@p
-r = n1 @ (B-I)
-
-print("Coordinates of point I:", I)
-print(f"Distance from I to BC= {r}")
